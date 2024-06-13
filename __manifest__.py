f0f6b2376e5b4f3cae2101a2da1af5fae856896f
AbdelrahmanElsheikh965 <abdoelsheikh892@gmail.com>
2024-06-13 03:58:27 +0300

    feat: added user access rights
    The user group has the following access rights:
    ➢Can create/read/update his own patients records
    ➢Can read only departments
    ➢Can read only doctors
    ➢Can’t view doctor fields in patients’ form view
    ➢Can’t view doctors’ menu item

--- __manifest__.py
+++ __manifest__.py
@@ -4,6 +4,9 @@
   'website': '',
   'description': 'Hospitals Management Systems',
   'data': [
+    'security/res_groups.xml',
+    'security/ir.model.access.csv',
+    # 'views/menu_items.xml', 
     'views/create_patients_data.xml',
     'views/create_departments_data.xml',
     'views/create_doctors_data.xml',
