566a200fd1cffd59fdb90fa0ae9a8f7d15f5d4dd
AbdelrahmanElsheikh965 <abdoelsheikh892@gmail.com>
2024-06-13 16:19:03 +0300

    feat: patient status report

--- __manifest__.py
+++ __manifest__.py
@@ -12,7 +12,9 @@
     'views/create_doctors_data.xml',
     'views/create_customer_patient.xml',
     'views/show_customer_website.xml',
-    'views/make_tax_id_required.xml'
+    'views/make_tax_id_required.xml',
+    'reports/patient_status_report.xml',
+    'reports/reports.xml'
   ],
   'installable': True,
   'auto_install': False,
